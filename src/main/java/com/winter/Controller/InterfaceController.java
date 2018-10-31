801ebcf265a68337f27cca38adcd8747add3de64
zhangfei88 <353062140@qq.com>
2018-10-31 14:35:29 +0800

    增加调用接口注释

--- src/main/java/com/winter/Controller/InterfaceController.java
+++ src/main/java/com/winter/Controller/InterfaceController.java
@@ -13,16 +13,22 @@ import java.util.Map;
 @Controller
 public class InterfaceController {
 
-
+    /**
+     * 调用第三方接口入口，可根据需求新建controller入口
+     * @param sessionId
+     * @param param
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "/trans", method = RequestMethod.POST)
     public String transInterface(String sessionId,String param){
-        String url="http://localhost:1111/user/add";
+        String url="http://localhost:1111/user/add"; // 调用第三方地址
+        //组织参数
         Map paramMap=new HashMap();
         paramMap.put("userName","哈哈1");
         paramMap.put("password","123456");
         paramMap.put("phone","1234561234");
-        return HttpClientUtil.sendHttpPost(url,paramMap);
+        return HttpClientUtil.sendHttpPost(url,paramMap);//在此选择map组织参数，util中有其他选择
     }
 
 }
